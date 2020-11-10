d1297a854a046cb282bb4ffd646f92154b3cb30d
Nikhil Dhawan <ndhawan593@gmail.com>
2020-11-10 20:41:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-     res.sendFile('index.html', {root: __dirname + '/public/'});
+     res.sendFile('home.html', {root: __dirname + '/public/'});
 });
 app.listen(process.env.PORT || 5000)
